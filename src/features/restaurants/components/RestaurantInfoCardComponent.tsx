bfef2af6feb8e26443847e8647eb34d56d66f210
simonedf <simonedifonzo91@gmail.com>
2021-11-27 00:08:31 +0000

    Migrating To Style Component

--- src/features/restaurants/components/RestaurantInfoCardComponent.tsx
+++ src/features/restaurants/components/RestaurantInfoCardComponent.tsx
@@ -4,12 +4,18 @@ import styled from "styled-components/native";
 import { Card } from "react-native-paper";
 
 //Styled Component
+const RestaurantCard = styled(Card)`
+  background-color: white;
+`;
+
+const RestaurantCardCover = styled(Card.Cover)`
+  padding: 20px;
+  background-color: white;
+`;
+
 const Title = styled(Text)`
   padding: 16px;
 `;
-
-const RestaurantCard = styled(Card)``;
-const RestaurantCardCover = styled(Card.Cover)``;
 
 export const RestaurantInfoCard = ({ restaurant = {} }) => {
   const {
@@ -25,14 +31,9 @@ export const RestaurantInfoCard = ({ restaurant = {} }) => {
   } = restaurant;
 
   return (
-    <Card elevation={5} style={styles.card}>
-      <Card.Cover key={name} style={styles.cover} source={{ uri: photos[0] }} />
+    <RestaurantCard elevation={5}>
+      <RestaurantCardCover key={name} source={{ uri: photos[0] }} />
       <Title>{name}</Title>
-    </Card>
+    </RestaurantCard>
   );
 };
-
-const styles = StyleSheet.create({
-  card: { backgroundColor: "white" },
-  cover: { padding: 20, backgroundColor: "white" },
-});
