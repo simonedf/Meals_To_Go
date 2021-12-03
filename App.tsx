94a3605831b538cd898af005977a09d8bf9cae0c
simonedf <simonedifonzo91@gmail.com>
2021-12-03 00:22:20 +0000

    Font Setup

--- App.tsx
+++ App.tsx
@@ -4,7 +4,28 @@ import { ThemeProvider } from "styled-components/native";
 import { RestaurantsScreen } from "./src/features/restaurants/screens/restaurantsScreen";
 import { theme } from "./src/infrastructure/theme";
 
+//Fonts Import
+import {
+  useFonts as useOswald,
+  Oswald_400Regular,
+} from "@expo-google-fonts/oswald";
+import { useFonts as useLato, Lato_400Regular } from "@expo-google-fonts/lato";
+
 export default function App() {
+  
+  //Load Font into APP
+  const [oswaldLoaded] = useOswald({
+    Oswald_400Regular,
+  });
+
+  const [latoLoaded] = useLato({
+    Lato_400Regular,
+  });
+
+  if (!oswaldLoaded || !latoLoaded) {
+    return null;
+  }
+
   return (
     <>
       <ThemeProvider theme={theme}>
