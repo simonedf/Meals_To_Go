5704213aa83b529269aa8b77811fa94697be2e06
simonedif <simonedf@nandos.co.uk>
2021-12-28 00:56:10 +0100

    Quick fix

--- src/services/restaurants/restaurantsService.tsx
+++ src/services/restaurants/restaurantsService.tsx
@@ -1,26 +1,23 @@
-import { mocks } from "./mock/index";
+import { mocks } from "./mock";
 import camelize from "camelize";
 
-export const restaurantsRequest = (location = "37.7749295,-122.4194155ppp") => {
+export const restaurantsRequest = (location = "37.7749295,-122.4194155") => {
   return new Promise((resolve, reject) => {
     const mock = mocks[location];
     if (!mock) {
-      reject("not Found");
+      reject("not found");
     }
     resolve(mock);
   });
 };
 
 const restaurantsTransform = ({ results = [] }) => {
-  const newResult = camelize(results.length);
-  return newResult;
-};
-
-restaurantsRequest()
-  .then(restaurantsTransform)
-  .then((transformedResponse) => {
-    console.log(transformedResponse);
-  })
-  .catch((err) => {
-    console.log("error");
+  const mappedResults = results.map((restaurant) => {
+    return {
+      ...restaurant,
+      isOpenNow: restaurant.opening_hours && restaurant.opening_hours.open_now,
+      isClosedTemporarily: restaurant.business_status === "CLOSED_TEMPORARILY",
+    };
   });
+  return camelize(mappedResults);
+};
