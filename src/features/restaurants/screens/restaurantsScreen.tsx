4e78019e7ca59b982685b4c723b88b49c6939034
simonedif <simonedf@nandos.co.uk>
2022-01-03 17:09:48 +0100

    activity indicator

--- src/features/restaurants/screens/restaurantsScreen.tsx
+++ src/features/restaurants/screens/restaurantsScreen.tsx
@@ -6,6 +6,16 @@ import { RestaurantInfoCard } from "../components/RestaurantInfoCardComponent";
 import { Spacer } from "../../../components/spacer/spacerComponent";
 import { SafeArea } from "../../../components/utility/safeAreaComponent";
 import { RestaurantsContext } from "../../../services/restaurants/restaurantsContex";
+import { ActivityIndicator, Colors } from "react-native-paper";
+
+const Loading = styled(ActivityIndicator)`
+  margin-left: -25px;
+`;
+const LoadingContainer = styled.View`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+`;
 
 const SearchContainer = styled.View`
   padding: ${(props) => props.theme.space[3]};
@@ -21,6 +31,11 @@ export const RestaurantsScreen = () => {
   const { isLoading, error, restaurants } = useContext(RestaurantsContext);
   return (
     <SafeArea>
+      {isLoading && (
+        <LoadingContainer>
+          <Loading size={50} animating={true} color={Colors.blue300} />
+        </LoadingContainer>
+      )}
       <SearchContainer>
         <Searchbar />
       </SearchContainer>
