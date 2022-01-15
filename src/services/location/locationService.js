1fac58f4c8a0a68ffc7b52028ed169694d5b731f
simonedf <simonedifonzo91@gmail.com>
2022-01-15 23:04:10 +0000

    Geocoding Service

--- src/services/location/locationService.js
+++ src/services/location/locationService.js
@@ -11,4 +11,10 @@ export const locationRequest = (searchTerm) => {
   });
 };
 
-export const locationTransform = (result) => {};
+export const locationTransform = (result) => {
+  const formattedResponse = camelize(result);
+  const { geometry = {} } = formattedResponse.results[0];
+  const { lat, lng } = geometry.location;
+
+  return { lat, lng };
+};
